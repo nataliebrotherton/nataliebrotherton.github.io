1d2fe43143162157bbc817d1b9ec86f1a6e2dc15
Natalie Brotherton <brothena@northvilleschools.net>
2018-12-23 22:37:38 -0500

    edit nav button

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
+function nav_bar() {
+  var nav_width = document.getElementById("sidenav").style.width;
+  if (nav_width === "0") {
+    openNav();
+  } else {
+    closeNav();
+  }
+}
+
 function openNav() {
   document.getElementById("sidenav").style.width = "100%";
 }
