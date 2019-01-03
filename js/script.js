492caad696dfb8e526283d253d64bc8720bb44cf
Natalie Brotherton <brothena@northvilleschools.net>
2019-01-03 11:29:40 -0500

    try to add portfolio images?

--- js/script.js
+++ js/script.js
@@ -3,58 +3,54 @@ $(document).ready(function() {
     $("#sidenav").slideToggle("fast");
   });
 
-  $(".works img").hover(function() {
-    var index = $(this).index();
-    var titles = [
-      "Pine bow",
-      "Forms sketch",
-      "574",
-      "Contour",
-      "Sketch",
-      "Contour",
-      "Gesture",
-      "Gesture",
-      "Inside/Outside",
-      "Inside/Outside",
-      "Hands gesture",
-      "Sketch",
-      "Figures - Peter",
-      "Figures - Peter",
-      "Figures - Peter",
-      "Figures - Peter",
-      "Figures - Louise",
-      "Figures - Louise",
-      "Value",
-      "Figures - Peter",
-      "Forms",
-      "Perspective",
-      "Figures - Louise"
-    ];
-    var desc = [
-      "Vine charcoal",
-      "Graphite",
-      "Graphite",
-      "Graphite, charcoal",
-      "Graphite, charcoal",
-      "Graphite, charcoal",
-      "Graphite",
-      "Graphite, charcoal",
-      "Ink",
-      "Ink, graphite",
-      "Graphite",
-      "Graphite, charcoal",
-      "Graphite",
-      "Graphite",
-      "Graphite",
-      "Graphite",
-      "Graphite",
-      "Graphite",
-      "Vine charcoal",
-      "Conte",
-      "Graphite",
-      "Ink",
-      "Ink, graphite, colored pencil"
-    ];
+  $("img.stamps, img.jamquiz, img.stock").hover(function() {
+    $(".project-container p").css({
+      'opacity' : '1'
+    });
+  });
+
+  // titles and descriptions to be on image overlays
+  var contentArray = new Array();
+  contentArray[0] = new Array("Pine bow", "Vine charcoal");
+  contentArray[1] = new Array("Forms sketch", "Graphite");
+  contentArray[2] = new Array("574", "Contour, graphite");
+  contentArray[3] = new Array("Contour", "Graphite, charcoal");
+  contentArray[4] = new Array("Sketch", "Graphite, charcoal");
+  contentArray[5] = new Array("Contour", "Graphite, charcoal");
+  contentArray[6] = new Array("Gesture", "Graphite");
+  contentArray[7] = new Array("Gesture", "Graphite, charcoal");
+  contentArray[8] = new Array("Inside/Outside", "Ink");
+  contentArray[9] = new Array("Inside/Outside", "Ink, graphite");
+  contentArray[10] = new Array("Hands gesture", "Graphite");
+  contentArray[11] = new Array("Sketch", "Graphite, charcoal");
+  contentArray[12] = new Array("Figures - Peter", "Graphite");
+  contentArray[13] = new Array("Figures - Peter", "Graphite");
+  contentArray[14] = new Array("Figures - Peter", "Graphite");
+  contentArray[15] = new Array("Figures - Peter", "Graphite");
+  contentArray[16] = new Array("Figures - Louise", "Graphite");
+  contentArray[17] = new Array("Figures - Louise", "Graphite");
+  contentArray[18] = new Array("Value study", "Vine charcoal");
+  contentArray[19] = new Array("Figures - Peter", "Conte");
+  contentArray[20] = new Array("Forms", "Graphite");
+  contentArray[21] = new Array("Perspective", "Ink");
+  contentArray[22] = new Array("Figures - Louise", "Ink, graphite, colored pencil");
+
+  // append image filename to its array
+  // create divs to hold images and place images inside them
+  var dir = "/static/assets/portfolio/";
+  var i;
+  for (i = 0; i < contentArray.length; i++) {
+    filename = dir + i + ".jpg";
+    contentArray[i][2] = filename;
+    $(".works").prepend('<div class="container'+ i +'" />');
+    var img = $('<img />').attr({
+            'id': i,
+            'class': 'piece',
+            'src': filename
+        }).prependTo('.container'+i);
+  }
+
+  $(".piece").mouseenter(function() {
     $(".overlay").css({
       'width' : $(this).css('width'),
       'height' : $(this).css('height'),
@@ -62,5 +58,13 @@ $(document).ready(function() {
       'left' : $(this).position().left,
       'opacity' : '0.6'
     });
+    $(".piece").mouseleave(function() {
+      $(".overlay").css({
+        'width' : $(this).css('width'),
+        'height' : $(this).css('height'),
+        'top' : $(this).position().top,
+        'left' : $(this).position().left,
+        'opacity' : '0'
+      });
   });
 });
